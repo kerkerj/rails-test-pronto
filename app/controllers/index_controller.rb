ae6fbae9443ea37677c3fe2a1aecbfdcb84b583c
kerkerj <jerryhuang@onead.com.tw>
2018-08-10 17:17:50 +0800

    add config

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,13 @@ class IndexController < ApplicationController
   def show
     render json: { index: true }
   end
+
+  private
+
+  def badCode
+    a=123
+    b=456
+
+    a = {:a => 1231231}
+  end
 end
